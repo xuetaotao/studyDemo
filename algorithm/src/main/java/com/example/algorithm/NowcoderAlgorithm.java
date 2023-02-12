aaec1a6e0bcf0723ff8225ec8347476225c5e030
taotao.xue@lotusflare.com <taotao.xue@lotusflare.com>
2023-02-12 22:20:17 +0800

    binarySearch practice

---
+++ algorithm/src/main/java/com/example/algorithm/NowcoderAlgorithm.java
@@ -0,0 +1,64 @@
+package com.example.algorithm;
+
+/**
+ * 牛客网算法篇：
+ * <a href="https://www.nowcoder.com/exam/oj?page=1&tab=%E7%AE%97%E6%B3%95%E7%AF%87&topicId=295">...</a>
+ */
+@SuppressWarnings("unused")
+public class NowcoderAlgorithm {
+
+    /**
+     * 二分查找/排序
+     * <p>
+     * BM17 二分查找-I
+     * 请实现无重复数字的升序数组的二分查找
+     * 时间复杂度O(logN),空间复杂度O(1)
+     */
+    public int bm17(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * BM18 二维数组中的查找
+     * 时间复杂度O(n+m): 遍历矩阵的时候，最多经过矩阵的一行一列
+     * 空间复杂度O(1):常数级变量，无额外辅助空间
+     * 思路：首先以左下角为起点，若是它小于目标元素，则往右移动去找大的，若是他大于目标元素，则往上移动去找小的。
+     */
+    public boolean bm18(int target, int[][] array) {
+        if (array.length == 0) {
+            return false;
+        }
+        //获取行数
+        int n = array.length;
+        //列数为0
+        if (array[0].length == 0) {
+            return false;
+        }
+        int m = array[0].length;
+        //从最左下角的元素开始往左或往上
+        for (int i = n - 1, j = 0; i >= 0 && j <= m - 1; ) {
+            //元素较大，往上走
+            if (array[i][j] > target) {
+                i--;
+            } else if (array[i][j] < target) {
+                //元素较小，往右走
+                j++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/studydemo/kotlinStudy/CoroutineDemo.kt
@@ -0,0 +1,48 @@
+package com.example.studydemo.kotlinStudy
+
+import com.example.studydemo.bean.UserBean
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+@SuppressWarnings("unused")
+class CoroutineDemo {
+
+    suspend fun suspendCoroutineDemo() {
+        suspendCoroutine<UserBean> { continuation ->
+            val stu = UserBean("aa", "bb", "cc", "dd", 2)
+            continuation.resume(value = stu)
+            continuation.resumeWith(Result.success(stu))
+            continuation.resumeWithException(Exception("hey"))
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    suspend fun suspendCancellableCoroutineDemo() {
+        suspendCancellableCoroutine<UserBean> { cancellableContinuation ->
+            val cancelled = cancellableContinuation.isCancelled
+            val stu = UserBean("aa", "bb", "cc", "dd", 2)
+            cancellableContinuation.resume(stu)
+            cancellableContinuation.resumeWith(Result.success(stu))
+            cancellableContinuation.resumeWithException(Exception("hey"))
+            cancellableContinuation.cancel(Exception("hey2"))
+        }
+        GlobalScope.launch {
+            //origin code use suspendCancellableCoroutine too.
+            delay(100)
+        }
+    }
+
+    class ContinuationImpl(override val context: CoroutineContext) : Continuation<UserBean> {
+        override fun resumeWith(result: Result<UserBean>) {
+            println()
+        }
+    }
+}
