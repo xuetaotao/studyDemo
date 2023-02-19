59566a900cafdc26f8e54aac5fe04732e5b1ed70
Taotao Xue <taotao.xue@lotusflare.com>
2023-02-19 18:58:48 +0800

    mergeSort

--- algorithm/src/main/java/com/example/algorithm/NowcoderAlgorithm.java
+++ algorithm/src/main/java/com/example/algorithm/NowcoderAlgorithm.java
@@ -83,4 +83,49 @@ public class NowcoderAlgorithm {
         //其中一个波峰
         return right;
     }
+
+
+    /**
+     * BM20 数组中的逆序对
+     * 时间复杂度O(nlogN), 空间复杂度O(n)
+     * 方法一：归并排序(TODO)
+     */
+    public int mod = 1000000007;
+
+    public int bm20(int[] array) {
+        int n = array.length;
+        int[] res = new int[n];
+        return mergeSort(0, n - 1, array, res);
+    }
+
+    public int mergeSort(int left, int right, int[] data, int[] temp) {
+        //停止划分
+        if (left >= right) {
+            return 0;
+        }
+        //取中间
+        int mid = (left + right) / 2;
+        //左右划分合并
+        int res = mergeSort(left, mid, data, temp) + mergeSort(mid + 1, right, data, temp);
+        //防止溢出
+        res %= mod;
+        int i = left, j = mid + 1;
+        for (int k = left; k <= right; k++) {
+            temp[k] = data[k];
+        }
+        for (int k = left; k <= right; k++) {
+            if (i == mid + 1) {
+                data[k] = temp[j++];
+            } else if (j == right + 1 || temp[i] <= temp[j]) {
+                data[k] = temp[i++];
+            } else {
+                //左边比右边大，答案增加
+                data[k] = temp[j++];
+                // 统计逆序对
+                res += mid - i + 1;
+            }
+        }
+        return res % mod;
+    }
+
 }
